2e68d4685375bc3c9b206b4d504ca64bd8c5b656
lutfullahsnr <128847239+lutfullahsnr@users.noreply.github.com>
2023-11-16 20:33:28 +0300

    Add files via upload

---
+++ 1xtoplam.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+     int a b;
+     printf("Lutfen bir sayi giriniz...");
+     scanf("%d", &a);
+     printf("Lutfen bir sayi giriniz...");
+     scanf("%d", &b);
+     int c=a+b;
+
+     scanf("%d", &c);
+     
+
+
+
+
+
+
+
+
+
+
+     return 0;
+}
